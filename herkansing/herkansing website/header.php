23c38ff7ede89e2ef61b8fc47cbc790900a76795
revskovic <144020309+revskovic@users.noreply.github.com>
2024-06-03 18:10:39 +0200

    Add files via upload

---
+++ herkansing/herkansing website/header.php
@@ -0,0 +1,7 @@
+<nav>
+    <a href="index.php">Home</a>
+    <a href="cursussen.php">Cursussen</a>
+    <a href="studenten.php">Studenten</a>
+    <a href="inschrijvingen.php">Inschrijvingen</a>
+    <a href="admin/logout.php">Logout</a>
+</nav>
